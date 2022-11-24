46721bba463825bf153e07b229fd345e835dc3a3
Okorie GoodGod <guddytechs@gmail.com>
2022-11-24 12:06:42 +0100

    I added the counter.js file to the code

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header/header';
 import Contact from './Component/contact';
+import Counter from './Component/counter';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Contact />
+      <Counter />
     </div>
 
   );
---
+++ my-react-app/src/Component/counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Counter = () => {
+    return (
+        <div>
+            <h1> Counter App</h1>
+            <p>Guddy Tech</p>
+            <button> Change Name</button>
+        </div>
+    )
+}
+
+export default Counter;
