4e1c17e849b3b7f6185eb8d50a0e683da9c14123
Okorie GoodGod <guddytechs@gmail.com>
2022-11-24 16:03:05 +0100

    I added the state management for the counter.js

--- my-react-app/src/Component/card.js
+++ my-react-app/src/Component/card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <div>
             <h2>{props.names}</h2>
-            <img src={props.img_path} alt="" width={200}/>
+            <img src={props.img} alt="" width={200}/>
             <p>phone no: {props.contact}</p>
             <p>{props.url}</p>
 
--- my-react-app/src/Component/counter.js
+++ my-react-app/src/Component/counter.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Counter = () => {
+        const [state, setState] = useState("Guddy");
+        const [count, setCount] = useState(0);
+
+        function changeName() {
+            setState("Praise");
+        }
+
+        function increaseValue() {
+            setCount(count + 1);
+        }
+
+        function decrementValue() {
+            setCount(count - 1);
+        }
+
     return (
         <div>
             <h1> Counter App</h1>
-            <p>Guddy Tech</p>
-            <button> Change Name</button>
+            <p>{state}</p>
+            <button onClick={changeName}> Change Name</button>
+            
+            <h1>{count}</h1>
+            <button onClick={decrementValue}>Decrement</button>
+
+            <button onClick={increaseValue}>Increment</button>
+
+            
+
+            
         </div>
     )
 }
